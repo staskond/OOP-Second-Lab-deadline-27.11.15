645eef679e8bfde5a32df695675b3fd12b219589
staskond <staskond@gmail.com>
2015-11-05 15:25:31 +0200

    серег, этот файл чекни, можно ли так конструктор запилить?

--- Photo.h
+++ Photo.h
@@ -8,9 +8,8 @@
 class Photo
 {
 public:
-	Photo(const std::string & _roadToFile, Date _date, Time _time, const std::sting & _commet);
-	Photo(const std::string & _roadToFile, Date _date, Time _time, const std::sting & _commet, Person );
-
+	Photo(const std::string & _roadToFile, Date(int _year, int _month, int _day), Time(const int _hour, const int _minutes), const std::string & _commet);//узнать, можно ли так делать?
+	//Photo(const std::string & _roadToFile, Date _date, Time _time, const std::sting & _commet, Person );
 private:
 
 };
