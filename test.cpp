606f3848c6a65e36d184e66650378801ea3efd7f
staskond <staskond@gmail.com>
2015-11-22 12:38:37 +0200

    не чего не изменилось

--- test.cpp
+++ test.cpp
@@ -1,8 +1,11 @@
 #include "Photo.h"
-
+#include <iostream>
 //Photo::Photo(const std::string & _roadTOFile, Date _year, Date _month, Date _Day, Time _hour, Time _minutes, const std::string & _commet)
 
 int main()
 {
-	Photo("D:\ ", 1997, 10, 2, 12, 15, "Коментарий")
+	Photo test1("D:\ ", 1997, 10, 2, 12, 15, "Коментарий");
+	std::cout << test1.GetYear;
+
+	Photo test2("D;\ ", Date(1997, 10, 2), Time(13, 15), "Коментарий");
 }
