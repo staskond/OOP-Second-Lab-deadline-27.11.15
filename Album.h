d4a7ba178f7e5a0145ae2eb5468088b619beed50
staskond <staskond@gmail.com>
2015-11-22 15:41:05 +0200

    почти сделано

--- Album.h
+++ Album.h
@@ -8,20 +8,19 @@ class Album
 public:
 	Album(const std::string & _AlbumName);
 	Album(const std::string & _AlbumName, std::initializer_list< Photo *> _photo);
-	~Album()
-	{
-		for (Photo * p_photo : m_photo)
-			delete p_photo;
-	}
+	~Album();
 	Album(const Album &) = delete;
 	Album & operator = (const Album &) = delete;
 	int getPhotoCount() const { return m_photo.size(); }//метод возвращающий количество фото в альбоме
 	void addPhoto(Photo * _photo);//метод добовл€ющий фото в конец альбома
+	void clearPhoto();
 
 
 
 private:
+	//название альбома
 	const std::string & m_AlbumName;
+	//набор фото
 	std::vector <Photo *> m_photo;
 };
 
