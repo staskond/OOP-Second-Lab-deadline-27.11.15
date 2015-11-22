3b42227f774ebdd14653ec477430c3d02a9ca0ce
staskond <staskond@gmail.com>
2015-11-22 14:16:31 +0200

    Добавлена реализация валидности фото

--- Photo.cpp
+++ Photo.cpp
@@ -1,24 +1,12 @@
 #include "Photo.h"
-/*
-Photo::Photo(Person _fullName, Person _Gender, Place _PlaceNamed, Place _city, Place _country)
-	:m_FullName(_fullName),
-	m_Gender(_Gender),
-	m_PlaceNamed(_PlaceNamed),
-	m_city(_city),
-	m_country(_country)
-{
-}
-Photo::Photo(const std::string & _roadTOFile, Date _year, Date _month, Date _Day, Time _hour, Time _minutes, const std::string & _commet)
-	:m_roadToFile(_roadTOFile),
-	m_year(_year),
-	m_month(_month),
-	m_day(_Day),
-	m_hour(_hour),
-	m_minutes(_minutes),
-	m_commet(_commet)
-{
-}*/
 
+void Photo::IsValidePhoto() const
+{
+	if (m_roadToFile.empty())
+		throw std::logic_error("Error: Road to file is not found.");
+	if (m_commet.empty())
+		throw std::logic_error("Error: Commet is empty.");
+}
 
 
 Photo::Photo(const std::string & _roadTOFile, Date _date, Time _time, const std::string & _commet)
@@ -27,6 +15,7 @@ Photo::Photo(const std::string & _roadTOFile, Date _date, Time _time, const std:
 	m_time(_time),
 	m_commet(_commet)
 {
+	IsValidePhoto();
 }
 
 
@@ -38,6 +27,7 @@ Photo::Photo(const std::string & _roadTOFile, Date _date, Time _time, std::vecto
 	m_person(_person),
 	m_commet(_commet)
 {
+	IsValidePhoto();
 }
 
 Photo::Photo(const std::string & _roadTOFile, Date _date, Time _time, std::vector <Place> _place, const std::string & _commet)
@@ -47,4 +37,5 @@ Photo::Photo(const std::string & _roadTOFile, Date _date, Time _time, std::vecto
 	m_place(_place),
 	m_commet(_commet)
 {
+	IsValidePhoto();
 }
