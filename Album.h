16c7bfe3595fb0a7b27e1a2f0c058e776ffb267f
staskond <staskond@gmail.com>
2015-11-22 16:34:04 +0200

    Переделана реализация с учетом "умных" указателей

--- Album.h
+++ Album.h
@@ -2,7 +2,7 @@
 #define _ALBUM_HPP_
 #include "Photo.h"
 #include <initializer_list>
-
+#include <memory>
 class Album
 {
 public:
@@ -11,17 +11,56 @@ public:
 	~Album();
 	Album(const Album &) = delete;
 	Album & operator = (const Album &) = delete;
-	int getPhotoCount() const { return m_photo.size(); }//метод возвращающий количество фото в альбоме
-	void addPhoto(Photo * _photo);//метод добовл€ющий фото в конец альбома
+	int getPhotoCount() const;//метод возвращающий количество фото в альбоме
+	void Album::addPhoto(std::unique_ptr< Photo > _photo);//метод добовл€ющий фото в конец альбома
 	void clearPhoto();
+	void removePhoto(Photo const & _photo);
+	int GetAlbumSize();
+	typedef std::vector< std::unique_ptr< Photo > >::const_iterator PhotoIterator;
+	/*https://github.com/zaychenko-sergei/oop-ki14/blob/master/lab2/demo_solution_14/employee.hpp */
+	PhotoIterator AlbumBegin () const ;
 
+	PhotoIterator AlbumEnd () const;
+	bool hasPhoto(Photo const & _Photo) const;
+	class IterablePhoto
+	{
+	public:
 
+		IterablePhoto(PhotoIterator _photoBegin, PhotoIterator _photosEnd)
+			: m_begin(_photoBegin), m_end(_photosEnd)
+		{}
+
+		PhotoIterator begin() const { return m_begin; }
+		PhotoIterator end() const { return m_end; }
+
+	private:
+
+		PhotoIterator m_begin, m_end;
+	};
+	
 
 private:
 	//название альбома
 	const std::string & m_AlbumName;
 	//набор фото
-	std::vector <Photo *> m_photo;
+	std::vector< std::unique_ptr< Photo > > m_photo;//храним по умным указзател€м 
+	//std::vector <Photo *> m_photo;
 };
 
+inline Album::PhotoIterator
+Album::AlbumBegin() const
+{
+	return m_photo.begin();
+}
+
+inline Album::PhotoIterator
+Album::AlbumEnd() const
+{
+	return m_photo.end();
+}
+
+inline int Album::getPhotoCount() const
+{
+	return m_photo.size();
+}
 #endif // _ALBUM_HPP_
