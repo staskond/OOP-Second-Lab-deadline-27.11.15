26a2d6b7b8f33fd7e402b63881d904bde5cdf4ed
staskond <staskond@gmail.com>
2015-11-23 03:59:02 +0200

    добавлен но не реализован

---
+++ Application.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef _APPLICATION_HPP_
+#define _APPLICATION_HPP_
+
+class  Application
+{
+public:
+	 Application();
+	 void test();
+	~ Application();
+
+private:
+
+};
+
+
+ Application:: Application()
+{
+}
+
+ Application::~ Application()
+{
+}
+
+#endif //_APPLICATION_HPP_
